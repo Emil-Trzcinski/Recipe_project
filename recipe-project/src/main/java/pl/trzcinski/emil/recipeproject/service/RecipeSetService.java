bae071110e5a5bf436c56bb5b0719be846df0769
Emil Trzcinski <trzcinski.emil@outlook.com>
2022-07-19 21:23:26 +0200

    founded bug, repaired it, added builder to test

--- recipe-project/src/main/java/pl/trzcinski/emil/recipeproject/service/RecipeSetService.java
+++ recipe-project/src/main/java/pl/trzcinski/emil/recipeproject/service/RecipeSetService.java
@@ -4,8 +4,10 @@ import pl.trzcinski.emil.recipeproject.model.Recipe;
 
 import java.util.Set;
 
-public interface RecipeSetService {
+public interface SetService {
 
     Set<Recipe> getSetOfRecipesWithAllParameters
             (int expectedKcal, int expectedTotalTimeMinutes, int numberOfMeals) throws Exception;
+
+    Set<Recipe> hasSameName(Set<Recipe> recipeTemp, Set<Recipe> mealsSet);
 }
